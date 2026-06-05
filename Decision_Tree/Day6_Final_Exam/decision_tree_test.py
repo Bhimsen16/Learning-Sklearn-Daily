433861f79a8c4e1444f249bd3c77c669e89f6759
Bhimsen16 <bhimsen1618@gmail.com>
2026-06-05 21:26:52 +0545

    Day 6: Completed Decision Tree exam on Wine Dataset

---
+++ Decision_Tree/Day6_Final_Exam/decision_tree_test.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
