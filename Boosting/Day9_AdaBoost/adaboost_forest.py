a94c95f5971665d70082eea26eb0d649f1beadcf
Bhimsen16 <bhimsen1618@gmail.com>
2026-06-09 17:41:18 +0545

    Upgraded script to compare AdaBoost hyperparameters

--- Boosting/Day9_AdaBoost/adaboost_forest.py
+++ Boosting/Day9_AdaBoost/adaboost_forest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 
 # Load the dataset and split it 70/30
@@ -10,16 +10,17 @@ X = pd.DataFrame(data = wine.data, columns = wine.feature_names)
 y = wine.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 42)
 
-# Initialize the model and train it
-forest = RandomForestClassifier(n_estimators = 100, max_depth = 1, random_state = 42)
-adaboost_forest = AdaBoostClassifier(n_estimators = 100, random_state = 42)
+# Configurations A: The Agressive Standard (100 estimators and full learning rate)
+standard_model = AdaBoostClassifier(n_estimators = 100, learning_rate = 1.0, random_state = 42)
+standard_model.fit(X_train, y_train)
+s_model_accuracy = accuracy_score(y_test, standard_model.predict(X_test))
 
-forest.fit(X_train, y_train)
-adaboost_forest.fit(X_train, y_train)
+# Configuration B: The Cautious Strategiest (50 estimators, smaller learning rate)
+cautious_model = AdaBoostClassifier(n_estimators = 50, learning_rate = 0.5, random_state = 42)
+cautious_model.fit(X_train, y_train)
+c_model_accuracy = accuracy_score(y_test, cautious_model.predict(X_test))
 
-# Print the test set accuracy
-forest_acc = accuracy_score(y_test, forest.predict(X_test))
-adaboost_acc = accuracy_score(y_test, adaboost_forest.predict(X_test))
-
-print(f"Random Forest Accuracy with max depth 1: {forest_acc * 100:.2f}%")
-print(f"AdaBoost Forest Accuracy with default max depth 1: {adaboost_acc * 100:.2f}%")
+# Print the Comparision Results
+print("--- ADABoost HyperParameter Test ---")
+print(f"Config A(100 estimators, Learning rate = 1.0) Accuracy: {s_model_accuracy * 100:.2f}%")
+print(f"Config B(50 estimators = 50, Learning rate = 0.5) Accuracy: {c_model_accuracy * 100:.2f}%")
